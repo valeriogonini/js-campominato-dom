1589963b1d0388c31659f4688efc9355389e9065
valeriogonini <valeriogonini@gmail.com>
2024-03-14 13:23:02 +0100

    add cell red

--- js/app.js
+++ js/app.js
@@ -16,6 +16,24 @@ console.log(CellGrid, ButtonPlay);
 
 
 ButtonPlay.addEventListener("click", function () {
+    const min = 1
+    let max = 100
+    const NumberRandom = 16
+
+    const arrayBomb = []
+
+    while (arrayBomb.length < NumberRandom) {
+        const NumberRandom = parseInt((Math.floor(Math.random() * max) + min))
+
+
+        if (arrayBomb.includes(NumberRandom) === false) {
+            arrayBomb.push(NumberRandom)
+
+        }
+
+    }
+    console.log(arrayBomb);
+
 
     const Select = document.getElementById("difficolta")
 
@@ -23,8 +41,10 @@ ButtonPlay.addEventListener("click", function () {
 
     CellGrid.innerHTML = ""
 
+
     if (Select.value === "difficolta1") {
         size = 10
+        
         CellGrid.classList.add("easy")
         CellGrid.classList.remove("medium", "hard")
 
@@ -56,9 +76,24 @@ ButtonPlay.addEventListener("click", function () {
         CellGrid.append(cellElement)
         // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro 
 
-        cellElement.addEventListener("click", function () {
 
-            cellElement.classList.toggle("bg-blue")
+        cellElement.addEventListener("click", function () {
+            
+            cellElement.classList.add("bg-blue")
+
+            for(let i = 0; i < arrayBomb.length; i++){
+                const CellBomb = arrayBomb[i]
+
+            if (parseInt(cellElement.innerHTML) === CellBomb){
+            cellElement.classList.add("bg-red")
+
+            }
+            }
+
+            
+
+              
+           
 
 
             // ed emetto un messaggio in console con il numero della cella cliccata.
@@ -67,5 +102,29 @@ ButtonPlay.addEventListener("click", function () {
 
 
         })
+
+
+
+
     }
+
 })
+
+
+
+
+
+
+// Descrizione:
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+// Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+// In seguito l’utente clicca su una cella:
+// se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso.
+// Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+// Attenzione che l’utente potrebbe cliccare due volte sulla stessa casella…
+
+
+
+
